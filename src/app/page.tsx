935067d4a7b93c54593235465f4e15b203beb75d
Yash Uppot <yashuppot@gmail.com>
2025-03-12 20:30:10 -0400

    Fix .env leak

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/ModeToggle";
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
+
+export default function Home() {
+  return (
+    <div className="m-4">
+      <h1>Home Page COntent</h1>
+    </div>
+  );
+}
