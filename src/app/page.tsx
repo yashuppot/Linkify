7d731a61a298a606105a7b188801145bd76c60cf
Yash Uppot <yashuppot@gmail.com>
2025-03-16 13:57:36 -0400

    Update UI to allow post actions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,13 @@ import CreatePost from "@/components/CreatePost";
 import FollowReccomended from "@/components/FollowReccomended";
 import PostCard from "@/components/PostCard";
 import { getPosts } from "@/actions/post.action";
+import { getDbUserId } from "@/actions/user.action";
 
 
 export default async function Home() {
   const user = await currentUser();
   const posts = await getPosts();
+  const dbUserId = await getDbUserId();
   console.log({posts});
   return (
     <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
@@ -18,7 +20,7 @@ export default async function Home() {
         {user ? <CreatePost /> : null}
         <div className="space-y-6"></div>
         {posts.map((post) => (
-          <PostCard key={post.id} post={post} />
+          <PostCard key={post.id} post={post} dbUserId={dbUserId} />
         ))}
       </div>
       <div className=" lg:col-span-4 sticky top-20">
--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,6 +1,55 @@
-import React from 'react'
+"use client"
+import { createComment, getPosts } from '@/actions/post.action'
+import { useUser } from '@clerk/nextjs'
+import React, { useState } from 'react'
+import toast from 'react-hot-toast'
 
-function PostCard() {
+type Posts = Awaited<ReturnType<typeof getPosts>>
+type Post = Posts[number];
+function PostCard({post, dbUserId} : {post:Post, dbUserId: string | null}) {
+    const { user } = useUser();
+    const [newComment, setNewComment] = useState("");
+    const [isCommenting, setIsCommenting] = useState(false);
+    const [isDeleting, setIsDeleting] = useState(false);
+    const [isLiking, setIsLiking] = useState(false);
+    const [hasLiked, setHasLiked] = useState(post.likes.some(like => like.userId === dbUserId));
+    const [optimisticLikes, setOptimisticLikes] = useState(post._count.likes);
+
+    const handleLike = async () => {
+        if(isLiking) return false;
+        try {
+            setIsLiking(true);
+            setHasLiked((prev) => !prev);
+            setOptimisticLikes((prev) => prev + (hasLiked ? -1 : 1));
+            await toggleLike(post.id);
+        } catch (error) {
+            setOptimisticLikes(post._count.likes);
+            setHasLiked(false);
+        }
+        finally{
+            setIsLiking(false);
+        }
+
+    };
+    const handleAddComment = async ()=> {
+        if (!newComment.trim() || isCommenting) return;
+    try {
+      setIsCommenting(true);
+      const result = await createComment(post.id, newComment);
+      if (result?.success) {
+        toast.success("Comment posted successfully");
+        setNewComment("");
+      }
+    } catch (error) {
+      toast.error("Failed to add comment");
+    } finally {
+      setIsCommenting(false);
+    }
+  };
+
+    const handleDeletePost = async () => {}
+
+
   return (
     <div>
       Post
@@ -9,3 +58,4 @@ function PostCard() {
 }
 
 export default PostCard
+
