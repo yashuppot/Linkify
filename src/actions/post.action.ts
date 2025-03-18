c4e02b853f3eff7f30efebf9349bf99560413a93
Yash Uppot <yashuppot@gmail.com>
2025-03-17 20:58:06 -0400

    Fix type error in post action

--- src/actions/post.action.ts
+++ src/actions/post.action.ts
@@ -4,24 +4,27 @@ import prisma from "@/lib/prisma";
 import { getDbUserId } from "./user.action";
 import { revalidatePath } from "next/cache";
 
-export async function createPost(content:string, imageUrl:string) {
-    try {
-        const userId = await getDbUserId();
-        const post = await prisma.post.create({
-            data:{
-                content: content,
-                image: imageUrl,
-                authorId: userId,
-            }
-        })
-        revalidatePath("/"); // clear homepage cache
-        return { success: true, post };
-    } catch (error) {
-        console.error("Failed to create post:", error);
-        return {success: false, error: "Failed to create post" };
-    }
-}
+export async function createPost(content: string, image: string) {
+  try {
+    const userId = await getDbUserId();
 
+    if (!userId) return;
+
+    const post = await prisma.post.create({
+      data: {
+        content,
+        image,
+        authorId: userId,
+      },
+    });
+
+    revalidatePath("/"); // purge the cache for the home page
+    return { success: true, post };
+  } catch (error) {
+    console.error("Failed to create post:", error);
+    return { success: false, error: "Failed to create post" };
+  }
+}
 export async function getPosts() {
     try {
         const posts = await prisma.post.findMany({
