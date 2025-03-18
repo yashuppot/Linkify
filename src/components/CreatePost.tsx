7edc709e74ae8bc8fee2aa4141d0d5832fb64bfa
Yash Uppot <yashuppot@gmail.com>
2025-03-17 20:43:30 -0400

    Add upload button

--- src/components/CreatePost.tsx
+++ src/components/CreatePost.tsx
@@ -8,6 +8,7 @@ import { Button } from './ui/button';
 import { ImageIcon, Loader2Icon, SendIcon } from 'lucide-react';
 import { createPost } from '@/actions/post.action';
 import toast from 'react-hot-toast';
+import ImageUpload from './ImageUpload';
 
 function CreatePost() {
   const {user}= nextjs.useUser();
@@ -52,7 +53,17 @@ function CreatePost() {
               onChange={(e) => setContent(e.target.value)}
               disabled={isPosting}
             />
-            {/*handle uploads */}
+            {(showImageUpload || imageUrl) && (
+              <div className="border rounded-lg p-4">
+                <ImageUpload
+                  endpoint='postImage'
+                  value={imageUrl}
+                  onChange={(url) => {
+                    setImageUrl(url);
+                    if(!url) setShowImageUpload(false);
+                  }} />
+              </div>
+            )}
         </div>
         
         <div className="flex items-center justify-between border-t pt-4">
